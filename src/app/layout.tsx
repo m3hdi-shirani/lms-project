6405540058432da73c0ecef9c25dcf98c283e264
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-28 08:31:05 +0330

    Add Iranyekan font files and implement Colors component for color display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,45 @@
+import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const figtree = Figtree({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
+
+const iranyekan = localFont({
+  src: [
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-DemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-iranyekan",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -6,10 +47,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="rtl">
+    <html dir="rtl" className={`${figtree.variable} ${iranyekan.variable}`}>
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-200 flex items-center justify-center">
-          Header
+          هدر سایت
         </header>
         <main className="flex-1 flex">{children}</main>
         <footer className="bg-gray-200 flex items-center justify-center">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Colors } from "./_components/colors/colors";
+
 export default function Home() {
   return (
     <div className="w-full text-5xl flex justify-center items-center">
-      Home Page
+      <Colors />
     </div>
   );
 }
