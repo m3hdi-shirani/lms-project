1280577441e351396781ec69eb6dcf574634e35b
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-29 17:14:59 +0330

    fix: correct import path for NavigationMenuItem type feat: add NavigationMenuItem type definition

--- src/app/_components/header/TopNavigation.tsx
+++ src/app/_components/header/TopNavigation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { NavigationMenuItem } from "@/types/NavigationMenuItem";
+import { NavigationMenuItem } from "@/types/navigation-menuItem";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
