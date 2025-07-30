4ef467ed36eb95f9e9b588bf9cef092d4681f728
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-30 15:41:07 +0330

    feat: add home features section to the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { CourseSummary } from "@/types/course-summery.interface";
 import HomeHeroSection from "./_components/home-hero-section/HomeHeroSection";
 import CourseCardList from "./(courses)/_components/CourseCardList";
+import { homeFeatures } from "@/data/home-feature";
+import Feature from "./_components/feature/Feature";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const response = await fetch(
@@ -18,6 +20,13 @@ export default async function Home() {
   return (
     <>
       <HomeHeroSection />
+      <section className="dark:bg-base-75 mt-10">
+        <div className="container flex flex-col lg:flex-row gap-10 xl:gap-5">
+          {homeFeatures.map((feature) => (
+            <Feature key={`feature-${feature.title}`} feature={feature} />
+          ))}
+        </div>
+      </section>
 
       <section className="container  pt-20">
         <div className="text-center xl:text-right">
