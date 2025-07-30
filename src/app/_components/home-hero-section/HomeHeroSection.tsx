170869b0c7aa546759b84d409f6b00502ac02dc0
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-30 09:06:39 +0330

    fix: adjust padding and layout in HomeHeroSection for better responsiveness

--- src/app/_components/home-hero-section/HomeHeroSection.tsx
+++ src/app/_components/home-hero-section/HomeHeroSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const HomeHeroSection: React.FC = () => {
   return (
-    <section className="bg-hero-pattern bg-no-repeat mt-5 xl:mt-20 xl:bg-left">
+    <section className="bg-hero-pattern bg-no-repeat mt-5 xl:mt-20 xl:bg-left px-4 xl:p-0">
       <div className="container flex flex-col-reverse items-center xl:flex-row">
         <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right">
           <h3 className="text-xl dark:text-info xl:text-2xl">
@@ -17,7 +17,7 @@ const HomeHeroSection: React.FC = () => {
             کلاسبن می‌تونی بدون محدودیت به قله‌های بالاتر صعود کنی. ما همیشه
             هواتو داریم.
           </p>
-          <div className="mt-5 flex gap-4">
+          <div className="mt-5 flex flex-col xl:flex-row gap-4">
             <Button variant="primary" size="large">
               دورهای نکست و ری اکت
             </Button>
