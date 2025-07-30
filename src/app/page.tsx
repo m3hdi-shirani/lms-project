48424b778df74ee0afaf3ae9239012f0eb288e4a
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-30 09:06:15 +0330

    feat: enhance Home component to display newest courses with CourseCardList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { CourseSummary } from "@/types/course-summery.interface";
 import HomeHeroSection from "./_components/home-hero-section/HomeHeroSection";
+import CourseCardList from "./(courses)/_components/CourseCardList";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const response = await fetch(
@@ -18,7 +19,15 @@ export default async function Home() {
     <>
       <HomeHeroSection />
 
-      <div className="container"></div>
+      <section className="container  pt-20">
+        <div className="text-center xl:text-right">
+          <h2 className="text-2xl font-extrabold">تازه ترین دوره های آموزشی</h2>
+          <p className="mt-3 text-lg">
+            برای به‌روز موندن، یاد گرفتن نکته‌های تازه ضروری‌ه!
+          </p>
+        </div>
+        <CourseCardList courses={newestCourses} />
+      </section>
     </>
   );
 }
