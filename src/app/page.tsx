e25ae22485645d443b0abb117169929b152573ce
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-30 07:57:02 +0330

    feat: implement getNewestCourses function to fetch latest courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { CourseSummary } from "@/types/course-summery.interface";
 import HomeHeroSection from "./_components/home-hero-section/HomeHeroSection";
 
-export default function Home() {
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+  const response = await fetch(
+    `https://api.classbon.com/api/courses/newest/${count}`
+  );
+  // if (!response.ok) {
+  //   throw new Error("Failed to fetch newest courses");
+  // }
+  return response.json();
+}
+
+export default async function Home() {
+  const newestCourses = await getNewestCourses(4);
+
   return (
     <>
       <HomeHeroSection />
+
+      <div className="container"></div>
     </>
   );
 }
