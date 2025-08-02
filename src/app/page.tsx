df99bdf84957ad434d9ecb705fab1ccf622abcf0
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-02 14:54:55 +0330

    feat: add testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import Feature from "./_components/feature/Feature";
 import HomeHeroSection from "./_components/home-hero-section/HomeHeroSection";
 import { IconArrowLeftFill } from "./_components/icons/icons";
 import { CardPlaceholder } from "./_components/placeholders";
+import { TestimonialList } from "./_components/testimonial/testimonial-list";
+import { testimonials } from "@/data/testimonials";
 
 async function getNewestPosts(count: number): Promise<BlogPostSummery[]> {
   const response = await fetch(`${API_URL}/blog/newest/${count}`);
@@ -105,6 +107,19 @@ export default async function Home() {
         </div>
         <BlogPostCardList posts={newestBlogPosts} />
       </section>
+
+      <div className="relative mt-32">
+        <div className="bg-primary pointer-events-none absolute bottom-0 left-1/2 aspect-square w-1/2 -translate-x-1/2 rounded-full opacity-5 -top-52 blur-3xl"></div>
+        <h2 className="text-info relative z-0 mx-auto text-3xl font-extrabold block w-fit">
+          <span className="-z-10 w-8 h-8 absolute bg-info opacity-25 -top-2 rounded-full inline-block -right-3"></span>
+          تجربه هم‌میسرهای کلاسبن
+        </h2>
+        <p className=" mb-32 text-lg text-center mt-2">
+          تو اینجا تنها نیستی. ببین هم‌مسیرهات نظرشون در مورد دوره‌های کلاسبن
+          چیه
+        </p>
+        <TestimonialList testimonials={testimonials} />
+      </div>
     </>
   );
 }
