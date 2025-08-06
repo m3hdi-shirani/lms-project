656bddbcabf9b7a9faebd2838a940616b3c11e9b
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-06 10:47:50 +0330

    refactor: replace useQuery with useInfiniteQuery in useCourseComments

--- src/app/(courses)/courses/[slug]/_api/get-comments.ts
+++ src/app/(courses)/courses/[slug]/_api/get-comments.ts
@@ -1,8 +1,8 @@
 import { readData } from "@/core/http-service/http-service";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { CourseCommentList } from "../_types/course-comment.interface";
-import { useQuery } from "@tanstack/react-query";
 
-type GetCommentOptions = {
+type GetCommentsOptions = {
   params: {
     slug: string;
     page: number;
@@ -11,19 +11,38 @@ type GetCommentOptions = {
 
 const getComments = ({
   params,
-}: GetCommentOptions): Promise<CourseCommentList> => {
+}: GetCommentsOptions): Promise<CourseCommentList> => {
   const { slug, page } = params;
   const url = `/courses/${slug}/comments?page=${page}`;
   return readData(url);
 };
 
-export const useCourseComments = ({ params }: GetCommentOptions) => {
-  const { data } = useQuery({
-    queryKey: ["courseComments"],
-    queryFn: () => getComments({ params }),
-    staleTime: 1000 * 60 * 60 * 5, // 5 hours
-    gcTime: 1000 * 60 * 60 * 6, // 6 hours
+export const useCourseComments = ({ params }: GetCommentsOptions) => {
+  const {
+    data,
+    error,
+    isFetchingNextPage,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  } = useInfiniteQuery({
+    queryKey: ["courseComments", params.slug],
+    queryFn: ({ pageParam }) =>
+      getComments({ params: { ...params, page: pageParam } }),
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+    initialPageParam: 1,
+    staleTime: 5 * 60 * 60 * 1000,
+    gcTime: 6 * 60 * 60 * 1000,
   });
 
-  return { data };
+  return {
+    data,
+    error,
+    isFetchingNextPage,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  };
 };
--- src/app/(courses)/courses/[slug]/_components/comments/CourseComment.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/CourseComment.tsx
@@ -1,25 +1,79 @@
 "use client";
 
-import { useParams } from "next/navigation";
-import { useCourseComments } from "../../_api/get-comments";
+import { Button } from "@/app/_components/button";
 import { Comment } from "@/app/_components/comment";
+import { IconRefresh } from "@/app/_components/icons/icons";
+import { TextPlaceholder } from "@/app/_components/placeholders";
+import { useParams } from "next/navigation";
+import { Fragment, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { useCourseComments } from "../../_api/get-comments";
 
-const CourseComment = () => {
+const CourseComments = () => {
+  const { ref, inView } = useInView({});
   const { slug } = useParams();
-  const { data: commants } = useCourseComments({
+
+  const {
+    data: comments,
+    error,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  } = useCourseComments({
     params: {
       slug: slug as string,
       page: 1,
     },
   });
 
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
+
+  if (error) {
+    return (
+      <>
+        <p>خطا در برقراری ارتباط با سرور</p>
+        <div className="text-center mt-3">
+          <Button
+            variant="neutral"
+            className="font-semibold"
+            isOutline={true}
+            shape="wide"
+            onClick={() => refetch()}
+          >
+            <IconRefresh />
+            تلاش مجدد
+          </Button>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
-      {commants?.data.map((comment) => (
-        <Comment key={`comment-${comment.id}`} {...comment} variant="info" />
+      {comments?.pages.map((currentPage) => (
+        <Fragment key={`comment-page-${currentPage}`}>
+          {currentPage.data.map((comment) => (
+            <Comment
+              key={`comment-${comment.id}`}
+              {...comment}
+              variant="info"
+            />
+          ))}
+        </Fragment>
       ))}
+
+      {(isFetching || hasNextPage) && (
+        <div ref={ref}>
+          <TextPlaceholder />
+        </div>
+      )}
     </>
   );
 };
 
-export default CourseComment;
+export default CourseComments;
