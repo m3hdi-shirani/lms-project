bb93f17da1dbf91eaf0071d55ad044a50e44cadb
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-06 11:45:49 +0330

    feat: Implement CourseCurriculum component

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -6,6 +6,8 @@ import { CourseDetails } from "@/types/course-details.interface";
 import { Tab } from "@/types/tab.types";
 import CourseAside from "./_components/course-aside/CourseAside";
 import CourseComment from "./_components/comments/CourseComment";
+import { CourseChapter } from "@/types/course-chapter.interface";
+import { CourseCurriculum } from "./_components/curriculum";
 
 export async function generateStaticParams() {
   const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
@@ -22,22 +24,31 @@ async function getCourse(slug: string): Promise<CourseDetails> {
   return res.json();
 }
 
+async function getCurriculum(slug: string): Promise<CourseChapter[]> {
+  const res = await fetch(`${API_URL}/courses/${slug}/curriculum`);
+  return res.json();
+}
+
 const CourseDetailsPage = async ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
-  const courseData = await getCourse(slug);
+  const courseData = getCourse(slug);
+  const courseCurriculumData = getCurriculum(slug);
 
-  const faqs: AccordionType[] = courseData.frequentlyAskedQuestions.map(
-    (faq) => ({
-      id: faq.id,
-      title: faq.question,
-      content: faq.answer,
-    })
-  );
+  const [course, courseCurriculum] = await Promise.all([
+    courseData,
+    courseCurriculumData,
+  ]);
+
+  const faqs: AccordionType[] = course.frequentlyAskedQuestions.map((faq) => ({
+    id: faq.id,
+    title: faq.question,
+    content: faq.answer,
+  }));
 
   const tabs: Tab[] = [
     {
       label: "مشخصات دوره",
-      content: courseData.description,
+      content: course.description,
     },
     {
       label: "دیدگاه‌ها و پرسش",
@@ -51,24 +62,29 @@ const CourseDetailsPage = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <div className=" container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
-      <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2 rounded-full opacity-10 blur-3xl"></div>
+      <div className="dark:bg-primary pointer-events-none absolute right-0 aspect-square w-1/2 rounded-full opacity-10 blur-3xl"></div>
       <div className="col-span-10 xl:col-span-7">
         <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
-          {courseData.title}
+          {course.title}
         </h1>
         <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
-          {courseData.subTitle}
+          {course.subTitle}
         </h2>
 
         <div className=" mt-5">Video Player Component</div>
       </div>
       <div className="col-span-10 xl:col-span-3">
-        <CourseAside {...courseData} />
+        <CourseAside {...course} />
       </div>
       <div className="col-span-10 xl:col-span-6 ">
         <Tabs tabs={tabs} />
       </div>
-      <div className="col-span-10 xl:col-span-4 bg-warning"></div>
+      <div className="col-span-10 xl:col-span-4 ">
+        <div className="sticky top-5">
+          <h2 className="mb-5 text-xl">سرفصل های دوره</h2>
+          <CourseCurriculum data={courseCurriculum} />
+        </div>
+      </div>
     </div>
   );
 };
