901a4cf739b65316c2a7812db6e8a5f1fcedbaaa
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-11 15:50:21 +0330

    refactor(textbox): refactor TextBox to support forwardRef and improve reusability

--- src/app/_components/textbox/Textbox.tsx
+++ src/app/_components/textbox/Textbox.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/display-name */
 import classNames from "classnames";
 import { Size } from "../types/size.type";
 import { TextboxProps } from "./textbox.types";
+import { forwardRef } from "react";
 
 const sizeClasses: Record<Size, string> = {
   tiny: "textbox-xs",
@@ -9,24 +11,23 @@ const sizeClasses: Record<Size, string> = {
   large: "textbox-lg",
 };
 
-const TextBox: React.FC<TextboxProps> = ({
-  variant = "ghost",
-  type = "text",
-  className,
-  size = "normal",
-  ...rest
-}: TextboxProps) => {
-  const classes = classNames(
-    "textbox",
-    "w-full",
-    className,
-    {
-      [`textbox-${variant}`]: variant,
-    },
-    { [`${sizeClasses[size]}`]: size }
-  );
+export const Textbox: React.FC<TextboxProps> = forwardRef<
+  HTMLInputElement,
+  TextboxProps
+>(
+  (
+    { variant = "ghost", type = "text", className, size = "normal", ...rest },
+    ref
+  ) => {
+    const classes = classNames(
+      "textbox",
+      "w-full",
+      className,
+      { [`textbox-${variant}`]: variant },
+      { [`${sizeClasses[size]}`]: size }
+    );
+    return <input ref={ref} type={type} className={classes} {...rest} />;
+  }
+);
 
-  return <input type={type} className={classes} {...rest} />;
-};
-
-export default TextBox;
+export default Textbox;
--- src/app/_components/textbox/textbox.types.ts
+++ src/app/_components/textbox/textbox.types.ts
@@ -1,7 +1,7 @@
 import { InputHTMLAttributes } from "react";
 import { ComponentBase } from "../types/component-base.type";
 
-type TextboxType = "text" | "email" | "password" | "number";
+type TextboxType = "text" | "number" | "email" | "password";
 
 export type TextboxProps = Omit<InputHTMLAttributes<HTMLInputElement>, "size"> &
   ComponentBase & {
