0c04f1207da95ed62d367d7ec3de084cb274d284
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-11 11:53:53 +0330

    feat: add Textbox component

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,12 +1,7 @@
-import Link from "next/link";
+import SignInForm from "./_components/SignInForm";
 
 const SigninPage = () => {
-  return (
-    <div className="w-full text-5xl flex flex-col justify-center items-center">
-      <h1>This is signin page</h1>
-      <Link href="/verify">Go to verification page</Link>
-    </div>
-  );
+  return <SignInForm />;
 };
 
 export default SigninPage;
