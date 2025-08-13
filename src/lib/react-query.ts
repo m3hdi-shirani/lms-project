809b1a42b69f27c248d8f0dcb74ada89d6e377e2
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-13 08:35:02 +0330

    feat: add error notifications for react-query mutations

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,3 +1,6 @@
+import { showNotification } from "@/store/notification.store";
+import { ProblemDetails } from "@/types/http-errors.interface";
+import { Notification } from "@/types/notification.interface";
 import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
@@ -8,8 +11,8 @@ export const queryClient = new QueryClient({
   }),
 
   mutationCache: new MutationCache({
-    onError: (error) => {
-      //show notification
+    onError: (error: unknown) => {
+      showNotifications(error as ProblemDetails);
     },
   }),
 
@@ -22,3 +25,24 @@ export const queryClient = new QueryClient({
     },
   },
 });
+
+const showNotifications = (problem: ProblemDetails) => {
+  const notifications: Omit<Notification, "id">[] = [];
+  if (problem?.errors) {
+    Object.entries(problem.errors).forEach(([_, values]) =>
+      values.forEach((errorMessage) =>
+        notifications.push({
+          message: errorMessage,
+          type: "error",
+        })
+      )
+    );
+  } else if (problem?.detail) {
+    notifications.push({
+      message: problem.detail,
+      type: "error",
+    });
+  }
+
+  showNotification(notifications);
+};
